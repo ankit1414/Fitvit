2676a7f0abf09f73d7439c1a6434074ef210eb24
Bhawna11agg <bhawna11agg@gmail.com>
2019-10-08 20:13:19 +0700

    removing merge conflicts

--- app/src/main/java/com/example/fitvit/MainActivity.java
+++ app/src/main/java/com/example/fitvit/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.fitvit;
-
 import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -124,6 +124,9 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_bmi) {
 
         } else if (id == R.id.nav_water_reminder) {
+
+            Intent i=new Intent (MainActivity.this,water_main.class);
+            startActivity(i);
 
         } else if (id == R.id.nav_exercise) {
 
---
+++ app/src/main/java/com/example/fitvit/water_main.java
@@ -0,0 +1,13 @@
+package com.example.fitvit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class water_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_main);
+    }
+}
