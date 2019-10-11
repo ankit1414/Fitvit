bbfe30f236f2860f1ec8f73ade5554210bb2658e
ankit1414 <bhardwajankit1414@gmail.com>
2019-10-12 00:42:21 +0530

    save and edit button active now

--- app/src/main/java/com/example/fitvit/BMIActivity.java
+++ app/src/main/java/com/example/fitvit/BMIActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -26,7 +27,11 @@ public class BMIActivity extends AppCompatActivity {
     Button save_height;
     Button save_weight;
 
-    TextView bmi_tv;
+    TextView bmi_tv, height_tv, weight_tv;
+    EditText height_et, weight_et;
+
+    SharedPreferences sharedPreferences;
+
 
 
     @Override
@@ -34,18 +39,9 @@ public class BMIActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
 
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.fitvit", Context.MODE_PRIVATE);
-        weightInKg = sharedPreferences.getFloat("weightInKg" , 80);
-        heightInCm = sharedPreferences.getFloat("heightInCm" , 160);
+        sharedPreferences = this.getSharedPreferences("com.example.fitvit", Context.MODE_PRIVATE);
+        calculateBMI();
 
-        float bmi = ((weightInKg)/((heightInCm*heightInCm)/(100*100)));
-        bmi = Float.parseFloat(df.format(bmi));
-
-        sharedPreferences.edit().putFloat("bmi" , bmi).apply();
-
-        bmi_tv = findViewById(R.id.tv_bmi_bmiActivity);
-
-        bmi_tv.setText(bmi+"");
 
         grp_height_edit = findViewById(R.id.height_edit);
         grp_weight_edit = findViewById(R.id.weight_edit);
@@ -58,11 +54,19 @@ public class BMIActivity extends AppCompatActivity {
         save_height = findViewById(R.id.save_button_height);
         save_weight = findViewById(R.id.save_button_weight);
 
+        height_tv = findViewById(R.id.height_bmi_activity);
+        weight_tv = findViewById(R.id.weight_bmi_activity);
+
+        height_et = findViewById(R.id.height_bmi_edit);
+        weight_et = findViewById(R.id.weight_bmi_edit);
+
         edit_height.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 grp_height_edit.setVisibility(LinearLayout.GONE);
                 grp_height_save.setVisibility(LinearLayout.VISIBLE);
+
+
             }
         });
 
@@ -77,18 +81,55 @@ public class BMIActivity extends AppCompatActivity {
         save_height.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                float newHeight = Float.parseFloat(height_et.getText().toString());
+                sharedPreferences.edit().putFloat("heightInCm" , newHeight).apply();
+
                 grp_height_edit.setVisibility(LinearLayout.VISIBLE);
                 grp_height_save.setVisibility(LinearLayout.GONE);
+                calculateBMI();
+
+
             }
         });
 
         save_weight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                float newWeight = Float.parseFloat(weight_et.getText().toString());
+                sharedPreferences.edit().putFloat("weightInKg" , newWeight).apply();
                 grp_weight_edit.setVisibility(LinearLayout.VISIBLE);
                 grp_weight_save.setVisibility(LinearLayout.GONE);
+                calculateBMI();
             }
         });
+
+
+
+    }
+    public void calculateBMI(){
+
+        sharedPreferences = this.getSharedPreferences("com.example.fitvit", Context.MODE_PRIVATE);
+
+        weightInKg = sharedPreferences.getFloat("weightInKg" , 0);
+        heightInCm = sharedPreferences.getFloat("heightInCm" , 1);
+
+        height_tv = findViewById(R.id.height_bmi_activity);
+        weight_tv = findViewById(R.id.weight_bmi_activity);
+
+        height_tv.setText(heightInCm + " cm ");
+        weight_tv.setText(weightInKg + " kg ");
+
+
+        float bmi = ((weightInKg)/((heightInCm*heightInCm)/(100*100)));
+        bmi = Float.parseFloat(df.format(bmi));
+
+        sharedPreferences.edit().putFloat("bmi" , bmi).apply();
+
+        bmi_tv = findViewById(R.id.tv_bmi_bmiActivity);
+
+        bmi_tv.setText(bmi+"");
 
 
 
