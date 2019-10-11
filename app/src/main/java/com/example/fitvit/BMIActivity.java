b8d731073e5951aa1534f7493b665ab0883d8712
ankit1414 <bhardwajankit1414@gmail.com>
2019-10-11 22:23:54 +0530

    edit button and save button added in bmi activity

--- app/src/main/java/com/example/fitvit/BMIActivity.java
+++ app/src/main/java/com/example/fitvit/BMIActivity.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -13,6 +16,15 @@ public class BMIActivity extends AppCompatActivity {
     float weightInKg;
     float heightInCm;
     private static DecimalFormat df = new DecimalFormat("0.00");
+
+    Button edit_height;
+    Button edit_weight;
+    LinearLayout grp_height_edit;
+    LinearLayout grp_weight_edit;
+    LinearLayout grp_height_save;
+    LinearLayout grp_weight_save;
+    Button save_height;
+    Button save_weight;
 
     TextView bmi_tv;
 
@@ -34,6 +46,50 @@ public class BMIActivity extends AppCompatActivity {
         bmi_tv = findViewById(R.id.tv_bmi_bmiActivity);
 
         bmi_tv.setText(bmi+"");
+
+        grp_height_edit = findViewById(R.id.height_edit);
+        grp_weight_edit = findViewById(R.id.weight_edit);
+        grp_height_save = findViewById(R.id.height_save);
+        grp_weight_save = findViewById(R.id.weight_save);
+
+        edit_height = findViewById(R.id.edit_button_height);
+        edit_weight = findViewById(R.id.edit_button_weight);
+
+        save_height = findViewById(R.id.save_button_height);
+        save_weight = findViewById(R.id.save_button_weight);
+
+        edit_height.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                grp_height_edit.setVisibility(LinearLayout.GONE);
+                grp_height_save.setVisibility(LinearLayout.VISIBLE);
+            }
+        });
+
+        edit_weight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                grp_weight_edit.setVisibility(LinearLayout.GONE);
+                grp_weight_save.setVisibility(LinearLayout.VISIBLE);
+            }
+        });
+
+        save_height.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                grp_height_edit.setVisibility(LinearLayout.VISIBLE);
+                grp_height_save.setVisibility(LinearLayout.GONE);
+            }
+        });
+
+        save_weight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                grp_weight_edit.setVisibility(LinearLayout.VISIBLE);
+                grp_weight_save.setVisibility(LinearLayout.GONE);
+            }
+        });
+
 
 
     }
